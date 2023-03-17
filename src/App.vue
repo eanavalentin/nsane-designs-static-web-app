cbaeddfbab20feaea41caee45d60626d1ebc8bc3
Valentin Eana <v.eana@ro.sotec.eu>
2023-03-17 12:24:51 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: null,
+    };
+  },
+  async mounted() {
+    const response = await fetch('/api/helloworld?name=nsane-designs');
+    this.message = await response.json();
+  },
+};
+</script>
