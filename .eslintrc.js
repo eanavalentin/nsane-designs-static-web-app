746791de64539ae8a23529c547917e12d7c12b17
Valentin Eana <v.eana@ro.sotec.eu>
2023-03-20 08:51:06 +0200

    Update settings, packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,18 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'no-shadow': ['error', { allow: ['state'] }],
+    'no-underscore-dangle': 'off',
+    'vue/no-template-shadow': 'off',
+    'import/extensions': 'off',
+    'vue/valid-v-slot': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
   },
 };
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,19 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'nSANE dESIGNs',
+    },
+  },
+  configureWebpack: {
+    watchOptions: {
+      ignored: /node_modules/,
+      poll: true,
+    },
+    experiments: {
+      topLevelAwait: true,
+    },
+  },
 });
